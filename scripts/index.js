153f03a6716d3d368343d89d5fd45aae0de8eccb
Yuliia <alice1yulia@gmail.com>
2023-09-28 17:06:01 +0200

    improved style

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+/**ADD NEW ELEMENT TO THE LIST JS DYNAMICLY, USING DOM */
+// const inputContainer = document.getElementsByClassName('input__container')[0]
+// const newItem = document.createElement('li');
+// newItem.textContent = "Hello";
+// inputContainer.appendChild(newItem);
+
