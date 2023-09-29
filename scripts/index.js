62b85730a7000b27d9aa2955ec64466a8d8b76ca
Yuliia <alice1yulia@gmail.com>
2023-09-29 23:47:33 +0200

    js changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,51 @@ const todoBody = document.getElementById("listInput");
 const todos = []; //свторили пустий масив в яки будемо додавати дані з інпута
 const inputElement = document.getElementById('inputData');//доступились до плейсхолдера за айді і вивели в змінну
 const newElements = document.getElementById('list') // доступились по айді до списку  вивели в змінну
+const filterCompleted = document.getElementById('todo__item--completed');
+
+function createTodoHTML(todo, index) {
+  return `<li class="todo__item" data-key="${index}">
+    <input type="checkbox" id="todo-${index}">
+    <label for="todo-${index}">${todo.name}</label>
+  </li>`
+}
+
+function renderTodos() {
+  todoBody.innerHTML = '';
+
+  todos.forEach((item, index) => {
+    todoBody.innerHTML += createTodoHTML(item, index)
+  })
+}
 
 inputElement.addEventListener("keyup", (e) => {
   if (e.key === 'Enter' || e.keyCode === 13) {
-    console.log(e.target.value);
-    //через івент лісенер додали подію
-    //на клік по клавіші Enter в поле інпута.
-
     const todo = {
       name: e.target.value,
       completed: false
-    }  // створили об'єкт в який будуть виводитись дані
+    }
 
-    todoBody.innerHTML += `
-  <li class="todo__item">
-    <input type="checkbox" id="item1">
-    <label for="item1">${todo.name}</label>
-  </li>`;
+    //додаємо об'єкти в масив todos
+    todos.push(todo);
+    renderTodos();
+    //очистили поле інтупа після натиснення ентер
+    e.target.value = " ";
+  }
+});
 
-    todos.push(todo);  //додаємо об'єкти в масив todos
-    console.log(todos);
-    e.target.value = " "; //очистили поле інтупа після натиснення ентер
+todoBody.addEventListener('click', (e) => {
+  const target = e.target;
+  const parent = target.parentNode;
+
+  if (parent.className !== 'todo__item') return;
+
+  if (target.tagName === 'INPUT') {
+    console.log(parent.dataset.key, todos[parent.dataset.key])
   }
 
-});
+=
+})
+
 
 
 
