df2fe4062c8e1b42cc04b426195a1003bb38120b
Yuliia <alice1yulia@gmail.com>
2023-10-01 01:00:25 +0200

    added counter, check&uncheck etc.

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,35 @@
 const todoBody = document.getElementById("listInput");
 const todos = []; //свторили пустий масив в яки будемо додавати дані з інпута
 const inputElement = document.getElementById('inputData');//доступились до плейсхолдера за айді і вивели в змінну
-const newElements = document.getElementById('list') // доступились по айді до списку  вивели в змінну
-const filterCompleted = document.getElementById('todo__item--completed');
+const newElements = document.getElementById('list'); // доступились по айді до списку  вивели в змінну
+const todoItem = document.getElementsByClassName('todo__item');
+const todoCount = document.getElementById('counter')
+const filtersContainer = document.querySelector('.todo__filters');// для фільтрів
 
+//функція динамічно додає елементи, на onclick змінює статус checked || unchecked
 function createTodoHTML(todo, index) {
-  return `<li class="todo__item" data-key="${index}">
-    <input type="checkbox" What needs to be here? id="todo-${index}">
+  return `<li class="todo__item ${todo.completed && 'checked'}" data-key="${index}">
+    <input
+      onclick="updateStatus(${index})"
+      type="checkbox" ${todo.completed && 'checked'}
+      id="todo-${index}"
+    >
     <label for="todo-${index}">${todo.name}</label>
-    <button class="custom-button"> delete </button>
   </li>`
 }
 
+/* <button class="custom-button"> delete</button> */
+
+
+//функція що змінює статус елементів якщо статус completed || !completed
+function updateStatus(index) {
+  todos[index].completed = !todos[index].completed;
+  renderTodos();
+  updatesCount();
+}
+
+
+//?
 function renderTodos() {
   todoBody.innerHTML = '';
 
@@ -20,18 +38,22 @@ function renderTodos() {
   })
 }
 
+// при натисненні на Enter додаються нові елементи, якщо інпут пустий,
 inputElement.addEventListener("keyup", (e) => {
-  if (e.key === 'Enter' || e.keyCode === 13) {
+  if ((e.key === 'Enter' || e.keyCode === 13) && e.target.value.trim() !== '') {
     const todo = {
       name: e.target.value,
       completed: false
     }
 
+
     //додаємо об'єкти в масив todos
     todos.push(todo);
     renderTodos();
+    updatesCount();
     //очистили поле інтупа після натиснення ентер
     e.target.value = " ";
+
   }
 });
 
@@ -45,9 +67,12 @@ todoBody.addEventListener('click', (e) => {
     console.log(parent.dataset.key, todos[parent.dataset.key])
   }
 
-
 })
-
-
+//ф-я показує скільки елементів, який треба виконати залишилось
+function updatesCount() {
+  const incompletedTasks = todos.filter(todos => !todos.completed).length;
+  todoCount.innerHTML = `${incompletedTasks} items left`
+  console.log("updatesCount", todoCount);
+}
 
 
